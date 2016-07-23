16f8c9b9d29ed3b769227b2ff377febd559a847c
Sivaranjani Subramanian <ranjani4ever@gmail.com>
2016-07-24 02:50:09 +0530

    Full code with no styling

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ActiveRecord::Base
+	belongs_to :user
+
+	validates :user_id, presence: true
+	validates :title, length: {minimum: 3 }, presence: true
+	validates :url, format: {with: Regexp.new(URI::regexp(%w(http https)))}, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :validatable
+
+  has_many :bookmarks
+  belongs_to :user
+end
